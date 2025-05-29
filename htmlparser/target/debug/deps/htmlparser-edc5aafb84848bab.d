8b49de8839e8d67f914311de0fa12d1b4bfe0903
Jeremy Smith <jeremy.ws.2008@gmail.com>
2025-05-29 16:59:34 +0000

    Added the gitignore I forgot

--- htmlparser/target/debug/deps/htmlparser-edc5aafb84848bab.d
+++
@@ -1,5 +0,0 @@
-/home/jerem/projdir/htmmod/htmlparser/target/debug/deps/libhtmlparser-edc5aafb84848bab.rmeta: src/lib.rs
-
-/home/jerem/projdir/htmmod/htmlparser/target/debug/deps/htmlparser-edc5aafb84848bab.d: src/lib.rs
-
-src/lib.rs:
